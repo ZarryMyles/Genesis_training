e2228bea468d149c0501386dfc9da852082ee33a
Mahanth <mahanth2709@gmail.com>
2021-04-14 11:32:47 +0530

    routing/homepage-skeleton-added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import "./App.css";
+import React, { Component, Fragment } from "react";
 import Homepage from "./component/homepage/homepage";
+import Catalog from "./component/Catalog/catalog";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Homepage />
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={homep} />
+          <Route path="/catalog" component={cat} />
+        </Switch>
+      </div>
+      </Router>
+    )
 }
+const homep = () => (
+  <Fragment>
+    <Homepage />
+  </Fragment>
+);
+const cat = () => (
+  <Fragment>
+    <Catalog />
+  </Fragment>
+);
+
+
 
 export default App;
--- src/component/homepage/homepage.js
+++ src/component/homepage/homepage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Catalog from "../Catalog/catalog";
+import {Link} from "react-router-dom";
 
 export default function homepage() {
   return (
     <div>
-      <Catalog />
+      <Link to="/catalog">Catalog</Link>
+      <h1>Title</h1>
     </div>
   );
 }
