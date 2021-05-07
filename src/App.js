0437ea3d787f8da6f39d56ea200a0d571ee3cc2c
richinrix <richinvishal@gmail.com>
2021-05-07 17:15:08 +0530

    toastify bug fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 // Other components
 import Navbar from "./component/Navbar";
--- src/component/Homepage.js
+++ src/component/Homepage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./css/homepage.css";
 import { Animated } from "react-animated-css";
 
